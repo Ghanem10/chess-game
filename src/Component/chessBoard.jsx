a622325f3ff0c1da02777f0e2c3963672e9043e8
Gani <ghanemalhamad10@gmail.com>
2023-01-26 18:37:43 +0300

    control piece image fixed

--- src/Component/chessBoard.jsx
+++ src/Component/chessBoard.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import displayPieces from "./layout/displayImage";
 
 
 export default function ChessBoard() {
     const [createSquares, setCreateSquare] = useState([]);
+    const Board = useRef(null);
     
     const Numbers_Verticlly = ['1', '2', '3', '4', '5', '6', '7', '8'];
     const Chars_Horizontally = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-
-    let GenerateRandomIndex = 0, count = 0;
-
+    let GenerateRandomIndex = 0, count = 0, isActivePiece = null;
+    
     function createBoard() {
         const Board = [];
 
@@ -23,9 +23,49 @@ export default function ChessBoard() {
         setCreateSquare(Board);
     }
 
-    useEffect(() => {
-        createBoard();
-    }, []);
+    function grabbingPiece(e) {
+        const el = e.target;
+
+        if (el.classList.contains('piece')) {
+            const x = e.clientX - 30;
+            const y = e.clientY - 30;
+
+            el.style.position = 'absolute';
+            el.style.left = `${x}px`;
+            el.style.top = `${y}px`;
+
+            isActivePiece = el;
+        }
+    }
+    
+    function MovingPiece(e) {
+        const Edges = Board.current;
+
+        if (isActivePiece && Edges) {
+            const MinX = Edges.offsetLeft + 9.15;
+            const MinY = Edges.offsetTop - 8.12;
+
+            const x = e.clientX - 30;
+            const y = e.clientY - 30;
+            
+            console.log(x, y)
+            isActivePiece.style.position = 'absolute';
+            
+            // Bug fix x and y
+            isActivePiece.style.left = x > MinX ? `${MinX}px` : `${x}px`;
+            isActivePiece.style.top = y < MinY ? `${MinY}px` : `${y}px`;
+           
+            isActivePiece.style.left = x < 214 ? `${214}px` : `${x}px`;
+            isActivePiece.style.top = y > 500 ? `${500}px` : `${y}px`;
+        }
+    }
+
+    function dropingPiece() {
+
+        if (isActivePiece) {
+            isActivePiece = null;
+        }
+    }
 
     const colorSwitch = (x) => {
         let res = x.slice(-1)[0];
@@ -40,12 +80,17 @@ export default function ChessBoard() {
         return ((color + GenerateRandomIndex) % 2 == 0) ? "white" : "darkblue";
     }
 
+    useEffect(() => {
+        createBoard();
+    }, []);
+
     return (
         <div className="chessBoard">
             {createSquares.map((row, index) => (
                 <div
                     className="row"
                     key={index}
+                    ref={Board}
                 >
                     {row.map((x, index) => (
                         (displayPieces(x)) 
@@ -53,14 +98,21 @@ export default function ChessBoard() {
                         (
                             <div
                                 key={index}
-                                className="square"
+                                className="square-piece"
                                 style={{
-                                    width: "59px",
-                                    height: "59px",
                                     backgroundColor: colorSwitch(x),
-                                    backgroundImage: `url(${displayPieces(x)})`,
                                 }}
                             >
+                                <div 
+                                    className="piece"
+                                    style={{
+                                        backgroundImage: `url(${displayPieces(x)})`,
+                                    }}
+                                    onMouseDown={(e) => grabbingPiece(e)}
+                                    onMouseMove={(e) => MovingPiece(e)}
+                                    onMouseUp={() => dropingPiece()}
+                                >
+                                </div>
                             </div>
                         ) 
                         : 
@@ -69,8 +121,6 @@ export default function ChessBoard() {
                                 key={index}
                                 className="square"
                                 style={{
-                                    width: "59px",
-                                    height: "59px",
                                     backgroundColor: colorSwitch(x),
                                 }}
                             >
