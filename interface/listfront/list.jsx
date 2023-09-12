9d50020af060f80f5652c7528b9bd25b15497ca1
gani <agani2032@gmail.com>
2023-09-13 01:22:06 +0300

    Setup initial websockt connection from client

--- interface/listfront/list.jsx
+++ interface/listfront/list.jsx
@@ -1,32 +1,44 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { faBoltLightning } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { LightContext } from '../../contextprovider/context.provider';
 import './list.scss';
 
-export default function ListOptions({ setStartGame }) {
+export default function ListOptions({ setStartGame, startGame }) {
 
     const { lightUI } = useContext(LightContext);
     const temp = useRef(null);
+    let ws;
     
     function toggleStartGame() {
-
         setStartGame(true);
         temp.current.classList.add('switch');
+
+        const serverURL = location.protocol;
+        const serverProtocol = serverURL.replace('http:', 'ws:');
+        const hostingServer = location.hostname;
+        const webSocketProtocol = serverProtocol + '//' + hostingServer;
+
+        if (!!ws) ws.close();
+        //TODO, implement closing conncetion after the game is over & sending moves to server.
+        ws = new WebSocket(webSocketProtocol);
+        ws.addEventListener("error", () => console.log("connection error"));
+        ws.addEventListener("open", () => console.log("connection established"));
+        ws.addEventListener("message", (msg) => console.log(`Message received: ${msg.data}`));
     }
 
     return (
-      <div className={`start ${ lightUI ? "light" : "" }`} ref={temp}>
-          <div className='game-type'>
-                <h1>Blitz Game</h1>
+      <section className={`start ${ lightUI ? "light" : "" }`} ref={temp}>
+          <header className='game-type'>
+                <h1 style={{ fontSize: "30px"}} >Blitz Game</h1>
                 <div className='game-timer'>
                     <FontAwesomeIcon id='icon' icon={faBoltLightning} /> <span>3</span>
                 </div>
-          </div>
+          </header>
           <div className='game-start-btn'>
-                <button className='new-game' onClick={toggleStartGame}>New Game</button>
+                <button className='new-game' onClick={() => toggleStartGame()}>New Game</button>
                 <button className='rematch'>Tournament</button>
           </div>
-      </div>
+      </section>
     );
 }
