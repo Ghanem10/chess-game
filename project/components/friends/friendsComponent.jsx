ddf5c37900ffdd06370a7994a3df5e35912a97fa
gani <agani2032@gmail.com>
2024-01-02 14:31:51 +0300

    Added friends component

--- project/components/friends/friendsComponent.jsx
+++ project/components/friends/friendsComponent.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
 
-export default function FriendsComponent({}) {
+export default function FriendsComponent({ user, data }) {
     return (
-        <div>friendsComponent</div>
+        <div className='profile-body-friends'>
+            <h1>Friends List</h1>
+            {
+                (user?.user.connectedfrineds.length > 0) ? (
+                    <div className='friends-list'>
+                        {
+                            user.map((friend, index) => (
+                                console.log(friend),
+                                    <div className='friend' key={index}>
+                                        <img 
+                                            src={friend.avatar} 
+                                            referrerPolicy="no-referrer" 
+                                        />
+                                        <span>{friend.name}</span>
+                                    </div>
+                                )
+                            )
+                        }
+                    </div>
+                ) : (
+                    <div className='friends-list'>
+                        {
+                            console.log(data)
+                        }
+                        {
+                            data.map((friend, index) => (
+                                    <div className='friend' key={index}>
+                                        <img 
+                                            src={friend.avatar} 
+                                            referrerPolicy="no-referrer" 
+                                        />
+                                        <span>{friend.name}</span>
+                                    </div>
+                                )
+                            )
+                        }
+                    </div>
+                )
+            }
+        </div>
     );
 }
