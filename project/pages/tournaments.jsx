cb8bd3772903f29a7c0c80ff5236710e39db4ae5
gani <agani2032@gmail.com>
2024-01-02 14:34:29 +0300

    Added Tournament component

--- project/pages/tournaments.jsx
+++ project/pages/tournaments.jsx
@@ -1,92 +1,157 @@
-
 import React, { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
 
-import '../../project/assets/scss/tournament.scss';
+import CreateTournament from '../components/tournaments/create.tournament';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { 
+    createTournamentWithId, 
+    getPublicTournaments, 
+    removeTournament 
+} from '../redux/actions/tournamentAction';
+
+import TournamentsMain from '../components/tournaments/tournaments.main';
+import TournamentHeaderMain from '../components/tournaments/header.main';
+import TournamentSidebarMain from '../components/tournaments/sidebar.main';
+import TestJSONData from '../data/tournaments.json';
+
+import '../assets/scss/tournaments/tournament.scss';
+
 
 export default function TournamentRooms() {
     
-    const [data, setData] = useState([]);
-    const [openMenu, setOpenMenu] = useState(false);
+    const testMod = localStorage.getItem("test") === "test" 
+    ? TestJSONData
+    : false;
 
-    const refTemplate = useRef(null);
+    const userTournaments = useSelector((state) => state.user?.tournaments);
+    const [tournamentToDelete, setTournamentToDelete] = useState([]);
 
-    const showTemplate = () => {
-        refTemplate.current.classList.add("showtemplate");
-        setOpenMenu(true);
+    const [showTournamentTemplate, setShowTournamentTemplate] = useState(false);
+    const [isDeleteTournament, setIsDeleteTournament] = useState(false);
+    const [saveButton, setSaveButton] = useState(false);
+
+    const [tournamentTitle, setTournamentTitle] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
+    const [error, setError] = useState("");
+
+    const dispatch = useDispatch();
+    
+
+    const showTemplate = () => { setShowTournamentTemplate(true); };
+    const hiddeTemplate = () => { setShowTournamentTemplate(false); };
+    const showTournaments = () => { setIsDeleteTournament(pre => !pre); };
+    const joinButton = () => {};
+    const inviteButton = () => {};
+
+
+    // Testing mod
+    const [testModBoolen, setTestModBoolen] = useState(false);
+
+    const switchToTestMode = () => {
+        localStorage.setItem("test", "test");
+        setTestModBoolen(pre => !pre);
     };
 
-    const cancelButton = () => {
-        refTemplate.current.classList.remove("showtemplate");
-        setOpenMenu(false);
+    const swtichToNormalMode = () => {
+        localStorage.removeItem("test");
+        setTestModBoolen(pre => !pre);
     };
 
+    const handleTournamentDataChange = (event) => {
+        setTournamentTitle(event.target.value);
+    };
+
+    const deleteTournaments = async () => {
+        const deletePromises = tournamentToDelete.map(
+            (tournamentId) => dispatch(removeTournament(tournamentId))
+        );
+
+        await Promise.all(deletePromises);
+    };
+    
+    const createTournament = async () => {
+        try {
+            const userId = JSON.parse(localStorage.getItem("token"))?.user._id;
+            await dispatch(createTournamentWithId(userId, tournamentTitle));
+            
+            setSaveButton(true);
+            setShowTournamentTemplate(false);
+        } catch (error) {
+            setError("The number of chars can't be greater than 10.");
+        }
+    };
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+    
     useEffect(() => {
         const getTournaments = async () => {
-            const token = JSON.parse(localStorage.getItem("token"));
-
-            const tournaments = await axios.get(
-                `${import.meta.env.VITE_URL}/tournament/public-tournaments`, {
-                headers: {
-                    Authorization: `Bearer ${token.accessToken}`,
-                },
-            });
-            setData(tournaments.data);
+            await dispatch(getPublicTournaments());
         };
         
         getTournaments();
-    }, []);
+    }, [saveButton]);
 
     return (
         <React.Fragment>
-            {openMenu && <div className='highlight'></div>}
+            
+            {showTournamentTemplate && (
+                <div className='highlight'></div>
+            )}
+
             <div className='main-tournament-section'>
-                <div className='tournaments-section'>
-                    <span>No tournaments available</span>
-                </div>
                 <div className='tournament-template-strc'>
+                    <div className='tournament-middle-section'>
+                        
+                        <TournamentHeaderMain 
+                            isDeleteTournament={isDeleteTournament}
+                            filterTournaments={handleSearchChange}
+                            deleteTournaments={deleteTournaments}
+                        />
+                        
+                        {userTournaments.length > 0 ? (
+                            <TournamentsMain 
+                                tournaments={userTournaments}
+                                searchTerm={searchTerm}
+                                joinButton={joinButton}
+                                isDeleteTournament={isDeleteTournament} 
+                                tournamentToDelete={tournamentToDelete}
+                                setTournamentToDelete={setTournamentToDelete}
+                            />
+                        ) : (testMod ? (
+                                <TournamentsMain 
+                                    tournaments={testMod}
+                                    searchTerm={searchTerm}
+                                    joinButton={joinButton}
+                                    isDeleteTournament={isDeleteTournament} 
+                                    tournamentToDelete={tournamentToDelete}
+                                    setTournamentToDelete={setTournamentToDelete}
+                                />
+                        ) : "No tournaments found." )}
+                    </div>
                     
-                    <div className='tournament-template' ref={refTemplate}>
-                        <label htmlFor="name">Tournament Name</label>
-                        <input type="text" name="name" id="" />
-                        <div className='tournaments'>
-                            {data.map((tournament) => (
-                                <div className='tournament' key={tournament.id}>
-                                    <div className='tournament-name'>
-                                        <span>{tournament.title}</span>
-                                    </div>
-                                    <div className='tournament-players'>
-                                        <span>{tournament.players}</span>
-                                    </div>
-                                    <div className='tournament-join'>
-                                        <button>Join</button>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                        <button className='invite-button' disabled>
-                            Invite
-                        </button>
-                        <div className='tournaments-buttons'>
-                            <button 
-                                onClick={cancelButton}
-                                className='button-cancel'
-                            >
-                                Cancel
-                            </button>
-                            <button>Save</button>
-                        </div>
-                    </div>
-                    <div className='buttons'>
-                        <button onClick={showTemplate} className='button-1'>
-                            Create new Tournament
-                        </button>
-                        <Link to={'/'}>
-                            <button className='button-2'>Main page</button>
-                        </Link>
-                    </div>
+                    <TournamentSidebarMain 
+                        showTournaments={showTournaments}
+                        showTemplate={showTemplate} 
+                        testModBoolen={testModBoolen}
+                        switchToTestMode={switchToTestMode}
+                        swtichToNormalMode={swtichToNormalMode}
+                    />
+
                 </div>
+
+                {showTournamentTemplate && (
+                    <CreateTournament 
+                        error={error}
+                        hiddeTemplate={hiddeTemplate} 
+                        inviteButton={inviteButton}
+                        joinButton={joinButton}
+                        createTournament={createTournament}
+                        handleTournamentDataChange={handleTournamentDataChange}
+                    />
+                )}
+                
             </div>
         </React.Fragment>
     );
