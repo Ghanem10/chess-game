5e0e926d003fefdd1bad55811de299b6eaf7b326
gani <agani2032@gmail.com>
2024-01-02 14:29:43 +0300

    Added responsive template on small screen

--- project/components/mobile/responsiveMobile.jsx
+++ project/components/mobile/responsiveMobile.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as Icon from 'react-bootstrap-icons';
 
-export default function ResponsiveMobile({}) {
+export default function ResponsiveMobile() {
+
+    const [show, setShow] = useState(false);
+
+    const handleShowList = () => setShow(true);
+    const handleShowChat = () => setShow(false);
+
     return (
         <div className='div-container'>
-            
+            <button onClick={handleShowList}>
+                <Icon.List className='List-icon'/>
+            </button>
+            <button onClick={handleShowChat}>
+                <Icon.Chat className='Chat-icon' />
+            </button>
+            <button>
+                <Icon.ArrowLeft className='Arrow-left-icon' />
+            </button>
+            <button>
+                <Icon.ArrowRight className='Arrow-right-icon' />
+            </button>
         </div>
     );
 }
